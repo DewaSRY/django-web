5ff657970ab580e461f7461f5e21b440934fe194
DewaSRY <sdewa6645@gmail.com>
2023-01-01 10:08:49 +0800

    after fix

--- Blog/settings.py
+++ Blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1ngkf7vyid&5-cjqkcnloy6142f)4#p4$&mw0a@bumdwzic)38'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv('IS_DEVELOPMENT',True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    getenv('APP_HOST' ,'localhost')
+]
 
 
 # Application definition
@@ -117,6 +120,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+# STATIC_ROOT=BASE_DIR /'staticfiles'
+
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[
